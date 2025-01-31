e8fbdccfa6c3fdfbe33ce0648921ca3a6678167b
barraotieno <barrackotieno160@gmail.com>
2025-01-31 17:59:19 +0300

    Feat: assigning ant to routes.

--- lem-in/assignants.go
+++ lem-in/assignants.go
@@ -5,42 +5,40 @@ import (
 	"strconv"
 )
 
-//Func Assignants() takes in the number of ants and the optimised path and assign each ant of the best path.
-
+// Func Assignants() takes in the number of ants and the optimised path and assign each ant of the best path.
 func Assignants(n int, optPaths [][]string) {
-	//rearrange the  paths with shortest at index 0.
+	// rearrange the  paths with shortest at index 0.
 	for i := 0; i < len(optPaths); i++ {
-		for j := i+1; j < len(optPaths); j++ {
+		for j := i + 1; j < len(optPaths); j++ {
 			if i+1 != len(optPaths) {
 				if len(optPaths[i]) > len(optPaths[j]) {
 					optPaths[i], optPaths[j] = optPaths[j], optPaths[i]
 				}
 			}
-		}		
+		}
 	}
 
 	trimmedPaths := [][]string{}
-	for i:=0; i < len(optPaths); i++ {
+	for i := 0; i < len(optPaths); i++ {
 		trimmedPaths = append(trimmedPaths, optPaths[i][1:])
 	}
 
-	routeLen := []int{}	
+	routeLen := []int{}
 	for i := 0; i < len(trimmedPaths); i++ {
 		routeLen = append(routeLen, len(trimmedPaths[i]))
 	}
 	antRoute := make([][]string, len(routeLen))
 
-
 	// for i := 1; i <= n; i++ {
-		
-		//assign first ant
-		// if i == 1 {
-		// 	antRoute = append(antRoute, []string{"L"+antNum})
-		// 	trimmedPaths[0] = append([]string{"X"}, trimmedPaths[0]...)
-		// 	routeLen[0]++
-		// 	continue
-		// }
-		antRoute = placeAnt(antRoute, trimmedPaths, routeLen, n)
+
+	// assign first ant
+	// if i == 1 {
+	// 	antRoute = append(antRoute, []string{"L"+antNum})
+	// 	trimmedPaths[0] = append([]string{"X"}, trimmedPaths[0]...)
+	// 	routeLen[0]++
+	// 	continue
+	// }
+	antRoute = placeAnt(antRoute, trimmedPaths, routeLen, n)
 	// }
 	fmt.Println("antroute:\n", antRoute)
 	fmt.Println("optPaths:\n", optPaths)
@@ -48,29 +46,24 @@ func Assignants(n int, optPaths [][]string) {
 	fmt.Println("routeLen:\n", routeLen)
 }
 
-
-func placeAnt(antRoute [][]string, trimmedPaths [][]string, routeLen []int, n int ) [][]string {
+func placeAnt(antRoute [][]string, trimmedPaths [][]string, routeLen []int, n int) [][]string {
 	if n == 0 {
 		return antRoute
 	}
 
 	antNum := strconv.Itoa(n)
-	
-	//check if routLen is leveled
+
+	// check if routLen is leveled
+	low := routeLen[0]
+	indx := 0
 	for i, l := range routeLen {
-		if i+1 != len(routeLen) {
-			if l < routeLen[i+1] {
-				antRoute[i] = append(antRoute[i], "L"+antNum)
-				trimmedPaths[i] = append([]string{"X"}, trimmedPaths[i]...)
-				routeLen[i]++				
-				break
-			} else if l > routeLen[i+1] {
-				antRoute[i] = append(antRoute[i], "L"+antNum)
-				trimmedPaths[i] = append([]string{"X"}, trimmedPaths[i]...)
-				routeLen[i]++				
-				break
-			} 
+		if l < low {
+			indx = i
 		}
 	}
+	antRoute[indx] = append(antRoute[indx], "L"+antNum)
+	trimmedPaths[indx] = append([]string{"X"}, trimmedPaths[indx]...)
+	routeLen[indx]++
+
 	return placeAnt(antRoute, trimmedPaths, routeLen, n-1)
 }
