9192f5c926214c5874adee6d03d96615df7fbb13
barraotieno <barrackotieno160@gmail.com>
2025-02-06 17:27:05 +0300

    Feat: link tag.

--- learn-groupie(api)/handlers/handlers.go
+++ learn-groupie(api)/handlers/handlers.go
@@ -9,12 +9,22 @@ import (
 )
 
 
+var Tmpl = template.Must(template.ParseGlob("templates/*.html"))
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseGlob("templates/*.html"))
 
-	err := tmpl.ExecuteTemplate(w, "index.html", datastructures.Artists)
+	err := Tmpl.ExecuteTemplate(w, "index.html", datastructures.Artists)
 	if err != nil {
+		log.Println("error parsing index.html templates.")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println("error parsing templates.")
+		return
+	}
+}
+
+func ArtistHandler(w http.ResponseWriter, r *http.Request) {
+	err:= Tmpl.ExecuteTemplate(w, "artist.html", datastructures.Artists[0])
+	if err != nil {
+		log.Println("Error passing artist.html templates")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
