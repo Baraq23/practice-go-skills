c01bb3c4c30d317d6d7c8cebf3f3ed8f60531955
barraotieno <barrackotieno160@gmail.com>
2025-02-06 11:37:06 +0300

    [feat:] serving data to the templates.

--- learn-groupie(api)/handlers/handlers.go
+++ learn-groupie(api)/handlers/handlers.go
@@ -2,14 +2,17 @@ package handlers
 
 import (
 	// "fmt"
+	"groupie/datastructures"
 	"html/template"
 	"log"
 	"net/http"
 )
 
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseGlob("templates/*.html"))
-	err := tmpl.ExecuteTemplate(w, "index.html", nil)
+
+	err := tmpl.ExecuteTemplate(w, "index.html", datastructures.Artists)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println("error parsing templates.")
