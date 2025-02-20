784010b94bd31e69b6a8676cb2f540aad579b5ba
enungo <nued65@gmail.com>
2025-02-20 11:35:47 +0300

    [core] log url link at start of the server.

--- forum/main.go
+++ forum/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"forum/handlers"
@@ -10,5 +11,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		handlers.HomeHandler(w, r)
 	})
+	log.Println("Server running on: http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
