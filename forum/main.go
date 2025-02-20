912c607726efa1fec1b53528eadbf0049d78416f
enungo <nued65@gmail.com>
2025-02-20 12:25:29 +0300

    [feat] users can input port number during program execution.

--- forum/main.go
+++ forum/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
 	"forum/handlers"
 )
 
 func main() {
+	args := os.Args
+	if len(args) > 2 {
+		fmt.Println("Usage: go run . [PORT]")
+	}
+	portStr := ":8080"
+	if len(args) == 2 {
+		portStr = args[1]
+		if strings.Contains(portStr, ":") {
+			portStr = portStr[1:]
+		}
+		portVal, err := strconv.Atoi(portStr)
+		if err != nil {
+			fmt.Println("Invalid port number input.")
+			return
+		}
+		if portVal < 1024 || portVal > 65535 {
+			fmt.Printf("Port :%d is not available. Available ports are between :1023 and :65536\n", portVal)
+			return
+		}
+		portStr = ":"+portStr
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		handlers.HomeHandler(w, r)
 	})
-	log.Println("Server running on: http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	log.Printf("Server running on: http://localhost%s\n", portStr)
+	http.ListenAndServe(portStr, nil)
 }
