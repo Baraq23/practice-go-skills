93b3333fb9194e562470173485295367064b8e71
enungo <nued65@gmail.com>
2025-02-20 11:14:21 +0300

    [fix] import package handlers, add arguments in HomeHanler

--- forum/main.go
+++ forum/main.go
@@ -1,11 +1,14 @@
 package forum
 
-import "net/http"
+import (
+	"net/http"
+
+	"forum/handlers"
+)
 
 func main() {
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handlers.HomeHandler()
+		handlers.HomeHandler(w, r)
 	})
 	http.ListenAndServe(":8080", nil)
 }
