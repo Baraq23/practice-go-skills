476d4850c01fc4ce781b8f1c12a25b234275b93a
Baraq23 <wolfgangkoppe18@gmail.com>
2025-02-27 18:07:07 +0300

    fix; print 2 values.

--- check-point/printmemory/main.go
+++ check-point/printmemory/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+// import "fmt"
+
+// func main() {
+// 	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+// }
+
+import (
+	"fmt"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/lib/random"
+	"github.com/01-edu/go-tests/solutions"
+)
 
 func main() {
-	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+	table := [10]byte{}
+
+	for j := 0; j < 5; j++ {
+		for i := 0; i < random.IntBetween(7, 10); i++ {
+			table[i] = byte(random.IntBetween(13, 126))
+		}
+		challenge.Function("PrintMemory", PrintMemory, solutions.PrintMemory, table)
+	}
+	table2 := [10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'}
+	challenge.Function("PrintMemory", PrintMemory, solutions.PrintMemory, table2)
 }
 
 func PrintMemory(arr [10]byte) {
@@ -46,6 +67,9 @@ func GetHex(r rune) string {
 		mod := r % 16
 		hex = string(h[mod]) + hex
 		r = r / 16
+	}
+	if len(hex) == 1 {
+		hex = "0"+hex
 	}
 	return hex
 }
