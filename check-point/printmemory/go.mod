91e71f4c729b1b82c34690fd288b4c84f1ac88f9
Baraq23 <wolfgangkoppe18@gmail.com>
2025-02-27 17:38:38 +0300

    feat; printmemory.

--- check-point/printmemory/go.mod
+++ check-point/printmemory/go.mod
@@ -1,3 +1,8 @@
 module printmemory
 
 go 1.23.4
+
+require (
+	github.com/01-edu/go-tests v0.0.0-20250124150613-b79f52cd2282 // indirect
+	github.com/01-edu/z01 v0.1.0 // indirect
+)
--- check-point/printmemory/main.go
+++ check-point/printmemory/main.go
@@ -1,57 +1,51 @@
 package main
 
-import (
-	"fmt"
-
-)
+import "fmt"
 
 func main() {
 	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
 }
 
-
-
 func PrintMemory(arr [10]byte) {
-    count := 0
-	for i:= range arr {
-		if arr[i] == 0 {
+	count := 0
+	for i, r := range arr {
+		if r == 0 {
 			fmt.Print("00")
 			count++
 		} else {
-			fmt.Print(GetHex(rune(arr[i])))
+			fmt.Print(GetHex(rune(r)))
 			count++
 		}
 		if count == 4 {
 			fmt.Println()
 			count = 0
 		} else {
-			if i < len(arr) - 1 {
+			if i < len(arr)-1 {
 				fmt.Print(" ")
 			} else {
 				fmt.Println()
 			}
 		}
 	}
+	fStr := ""
+	for _, r := range arr {
+		if r >= 32 && r <= 126 {
+			fStr += string(r)
+		} else {
+			fStr += "."
+		}
+	}
+	fmt.Println(fStr)
 }
-
 
 func GetHex(r rune) string {
 	hex := ""
 	h := "0123456789abcdef"
 
 	for r != 0 {
-		mod := r%16
-		hex=string(h[mod])+hex
-		r = r/16
+		mod := r % 16
+		hex = string(h[mod]) + hex
+		r = r / 16
 	}
 	return hex
 }
-
-// Output
-
-// $ go run . | cat -e
-// 68 65 6c 6c$
-// 6f 10 15 2a$
-// 00 00$
-// hello..*..$
-// $
